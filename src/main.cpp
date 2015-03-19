8659ecd765e5f120f7932a1cda01890ff65806d4
grantcoin <mail@grantcoin.org>
2015-03-19 18:19:47 -0400

    change epoch time & nNonce

--- src/main.cpp
+++ src/main.cpp
@@ -2271,7 +2271,7 @@ bool LoadBlockIndex(bool fAllowNew)
         // Genesis block
         const char* pszTimestamp = "CSRwire 27-FEB-2015 Morgan Stanley Survey Finds Sustainable Investing Poised for Growth";
         CTransaction txNew;
-        txNew.nTime = 1426148439;
+        txNew.nTime = 1426801523;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(9999) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
@@ -2281,9 +2281,9 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1426153587;
+        block.nTime    = 1426803487;
         block.nBits    = bnProofOfWorkLimit.GetCompact();
-        block.nNonce   = 346449998;
+        block.nNonce   = 0;
 
         if (fTestNet)
         {
