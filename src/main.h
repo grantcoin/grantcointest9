0dc87a7dc250e2ace4f96e49327becf3620609d3
grantcoin <mail@grantcoin.org>
2015-03-20 18:55:40 -0400

    change genesis block hash

--- src/main.h
+++ src/main.h
@@ -53,7 +53,7 @@ static const int fHaveUPnP = true;
 static const int fHaveUPnP = false;
 #endif
 
-static const uint256 hashGenesisBlockOfficial("0x0000000807108c8bfd86ed11620cd439b4d693b1c3eab41d27a9786b562839d0");
+static const uint256 hashGenesisBlockOfficial("0x0000000d7fbbe584ad417d8abe92516fe89e94b97a381c298c489e6b8e34aba0");
 static const uint256 hashGenesisBlockTestNet("0x00000001f757bb737f6596503e17cd17b0658ce630cc727c0cca81aec47c9f06");
 
 static const int64 nMaxClockDrift = 2 * 60 * 60;        // two hours
