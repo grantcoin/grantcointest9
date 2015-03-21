e5bb7294dc134bb584696a683da1abfd6645a473
grantcoin <mail@grantcoin.org>
2015-03-20 23:28:20 -0400

    change coinbase maturity to PPCoin original

--- src/main.h
+++ src/main.h
@@ -38,7 +38,7 @@ static const int64 MIN_RELAY_TX_FEE = CENT;
 static const int64 MAX_MONEY = 50000000000 * COIN;
 static const int64 MIN_TXOUT_AMOUNT = MIN_TX_FEE;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
-static const int COINBASE_MATURITY_GRT = 100;
+static const int COINBASE_MATURITY_GRT = 500;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 static const int STAKE_TARGET_SPACING = 1.5 * 60; // 90-second block spacing 
