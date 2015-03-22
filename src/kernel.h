82d4f9a55fa5013deb3da7559db720a912e1ec57
grantcoin <mail@grantcoin.org>
2015-03-22 00:10:34 -0400

    change stake modifier interval

--- src/kernel.h
+++ src/kernel.h
@@ -8,7 +8,7 @@
 #include "main.h"
 
 // MODIFIER_INTERVAL: time to elapse before new modifier is computed
-static const unsigned int MODIFIER_INTERVAL = 3 * 60 * 60;  // 3 hours
+static const unsigned int MODIFIER_INTERVAL = 1 * 60 * 60;  // 1 hour
 extern unsigned int nModifierInterval;
 
 // MODIFIER_INTERVAL_RATIO:
